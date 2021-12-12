a4cde4092e69c7ff5c0b043085e1dfb169a4a845
Abhishek Dhanani <abhishek9712a@gmail.com>
2021-12-13 01:01:08 +0530

    add logo

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
 import 'package:copyrightapp/const/svg_const.dart';
 import 'package:copyrightapp/pages/setting/setting.dart';
+import 'package:copyrightapp/pages/show_image/shaow_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:developer' as developer;
+import 'package:image/image.dart' as ui;
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -13,6 +20,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late File _originalImage;
+  late File _watermarkImage;
+  File? _watermarkedImage;
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -37,17 +52,25 @@ class _HomePageState extends State<HomePage> {
                     children: <Widget>[
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: const <Widget>[
-                          CenterIconWidget(
-                            backgroundImage: SVGConst.rectangleBlue,
-                            centerdImage: SVGConst.camera,
-                            title: "Take a Photo",
+                        children: <Widget>[
+                          InkWell(
+                            onTap: () async {
+                              await takeAPhoto();
+                            },
+                            child: const CenterIconWidget(
+                              backgroundImage: SVGConst.rectangleBlue,
+                              centerdImage: SVGConst.camera,
+                              title: "Take a Photo",
+                            ),
                           ),
-                          SizedBox(width: 20),
-                          CenterIconWidget(
-                            backgroundImage: SVGConst.rectanglYellow,
-                            centerdImage: SVGConst.file,
-                            title: "Open Gallery",
+                          const SizedBox(width: 20),
+                          InkWell(
+                            onTap: openGallery,
+                            child: const CenterIconWidget(
+                              backgroundImage: SVGConst.rectanglYellow,
+                              centerdImage: SVGConst.file,
+                              title: "Open Gallery",
+                            ),
                           ),
                         ],
                       )
@@ -61,6 +84,58 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  Future<void> openGallery() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _originalImage = File(image.path);
+        _watermarkImage = File(image.path);
+      });
+    }
+    developer.log(
+      image?.path ?? 'No picker Any Immage',
+      name: 'openGallery Function',
+    );
+
+    File? withLogo = await setWaterMark(_originalImage, _watermarkImage, "hii");
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const ShowImage(),
+      ),
+    );
+  }
+
+  Future<File?> setWaterMark(File _originalImage, File _watermarkImage,
+      String _waterMarkString) async {
+    ui.Image? originalImage = ui.decodeImage(_originalImage.readAsBytesSync());
+    ui.Image? watermarkImage =
+        ui.decodeImage((_watermarkImage.readAsBytesSync()));
+    if (originalImage != null && watermarkImage != null) {
+      //* Get Height and width
+      ui.Image image = ui.Image(originalImage.width, originalImage.height);
+      //* Drow Image
+      ui.drawImage(image, watermarkImage);
+      //*  setString
+      ui.drawString(
+        originalImage,
+        ui.arial_24,
+        originalImage.width - 300,
+        originalImage.height - 100,
+        _waterMarkString,
+      );
+      final File watermarkedFile =
+          File('${(await getTemporaryDirectory()).path}/xyz');
+      await watermarkedFile.writeAsBytes(ui.encodeJpg(originalImage));
+      return watermarkedFile;
+    }
+  }
+}
+
+Future<void> takeAPhoto() async {
+  debugPrint("hii");
 }
 
 class PageAppBar extends StatelessWidget {
@@ -74,29 +149,36 @@ class PageAppBar extends StatelessWidget {
       padding: const EdgeInsets.only(top: 50, left: 18, right: 18),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const <Widget>[
-              Text(
-                "Select Opetion",
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                "Select opetion to apply copyright on Image",
-                style: TextStyle(
+          SizedBox(
+            width: MediaQuery.of(context).size.width - 45 - 38,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                Text(
+                  "Select Opetion",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "Select opetion to apply copyright on Image",
+                  style: TextStyle(
                     fontSize: 13,
                     color: Color(0xFFBAB8F1),
-                    fontWeight: FontWeight.w600),
-              )
-            ],
+                    fontWeight: FontWeight.w600,
+                  ),
+                  softWrap: true,
+                  overflow: TextOverflow.clip,
+                )
+              ],
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(right: 10),
